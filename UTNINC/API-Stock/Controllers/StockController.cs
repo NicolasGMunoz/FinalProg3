425fd0dea4690aacfe2715552eba3fc4a6bf1b90
Nicolas G Munoz <nicolasgustavomunoz@gmail.com>
2024-06-19 14:49:56 -0300

    first commit

---
+++ UTNINC/API-Stock/Controllers/StockController.cs
@@ -0,0 +1,42 @@
+﻿using APIStock.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UTN.Inc.Entities;
+
+
+namespace APIStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public StockController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{productoId}")]
+        public async Task<ActionResult<int>> GetStock(int productoId)
+        {
+            var producto = await _context.Producto.FindAsync(productoId);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            var compras = await _context.Compra
+                .Where(c => c.ProductoId == productoId)
+                .SumAsync(c => c.Cantidad);
+
+            var ventas = await _context.Venta
+                .Where(v => v.ProductoId == productoId)
+                .SumAsync(v => v.Cantidad);
+
+            var stock = compras - ventas;
+
+            return Ok(stock);
+        }
+    }
+}
